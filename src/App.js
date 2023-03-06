af00c118870ee8b7f3e2640520d0b03a13f44aba
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-03-06 15:53:47 +0700

    Fixed DISPUTED style

--- src/App.js
+++ src/App.js
@@ -261,7 +261,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
-        {isDisputed ? <span className="disputed">[⛔️ DISPUTED}</span> : null}
+        {isDisputed ? <span className="disputed">[⛔️ DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
